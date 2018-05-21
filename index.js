49b92b99b62fa45625724e749c079c40305108c3
Cindy Vo <yendai.vo@hotmail.com>
2018-05-21 15:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `Let's talk about ${topic}`;
+>>>>>>> 949450c0d4ce1aa501bfc970a1eb005529b96575
